4d6d27a04db921e7d104efcb0db78969cb16ced5
Kostis Bourlas <kostisbourlas@protonmail.com>
2024-02-21 13:31:36 +0200

    Add functionality for counting file lines

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -8,26 +9,52 @@ import (
 
 func Execute() {
     countFlag := flag.Bool("c", false, "Size of file in Bytes")
+    linesFlag := flag.Bool("l", false, "Number of lines in file")
+
     file := os.Args[2]
     flag.Parse()
 
-    var size int 
-    var err error
     if *countFlag == true {
-	size, err = getFileSize(file)
+	size, err := getFileSize(file)
 	if err != nil {
 	    fmt.Println(err)
 	    os.Exit(1)	
 	}
+	fmt.Printf("%d %s\n", size, file)
+	os.Exit(0)
     }
-    fmt.Printf("%d %s\n", size, file)
-    os.Exit(0)
+
+    if *linesFlag == true {
+	lineCount, err := countLines(file)
+	if err != nil {
+	    fmt.Println(err)
+	    os.Exit(1)
+	}
+	fmt.Printf("%d %s\n", lineCount, file)
+	os.Exit(0)
+    }
 }
 
-func getFileSize(file string) (int, error) {
-    fileStats, err := os.Stat(file)
+func getFileSize(fileName string) (int, error) {
+    fileStats, err := os.Stat(fileName)
     if err != nil {
 	return 0, err 
     }
     return int(fileStats.Size()), nil
+}
+
+func countLines(fileName string) (int, error) {
+    file, err := os.Open(fileName)
+    if err != nil {
+	return 0, err
+    }
+    defer file.Close()
+
+    var lineCount int
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+	lineCount++
+    }
+
+    return lineCount, nil
 }
